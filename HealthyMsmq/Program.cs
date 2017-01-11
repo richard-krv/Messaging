48490d9fb1998adbe213f392cd323f11f10d6998
Richard K <richard.krv@gmail.com>
2017-01-11 06:41:14 +0200

    freeing the lib, adjusted comments

--- HealthyMsmq/Program.cs
+++ HealthyMsmq/Program.cs
@@ -7,13 +7,14 @@ namespace HealthyMsmq
     {
         static void Main(string[] args)
         {
+            var msmq = IntPtr.Zero;
             try
             {
-                Console.WriteLine("Press any key to load the Mqrt.dll library");
+                Console.WriteLine("Press any key to check if MSMQ is installed");
                 Console.ReadKey();
 
-                var lres = LoadLibrary("Mqrt.dll");
-                Console.WriteLine("Done with result {0}", lres);
+                msmq = LoadLibrary("mqrt.dll");
+                Console.WriteLine("MSMQ is {0}installed", msmq == IntPtr.Zero? "not " : "");
             }
             catch (Exception ex)
             {
@@ -21,12 +22,15 @@ namespace HealthyMsmq
             }
             finally
             {
-                Console.WriteLine("Press any key to exit ...");
-                Console.ReadKey();
+                FreeLibrary(msmq);
             }
-        }
+            Console.WriteLine("Press any key to exit ...");
+            Console.ReadKey();
+       }
 
         [DllImport("kernel32.dll")]
         public static extern IntPtr LoadLibrary(string dllToLoad);
+        [DllImport("kernel32.dll")]
+        public static extern bool FreeLibrary(IntPtr hModule);
     }
 }
